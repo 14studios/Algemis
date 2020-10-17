5c19fbc077c8f55153111f06828a6babe42f20a6
justinl681 <59315005+justinl681@users.noreply.github.com>
2020-10-17 19:27:12 -0400

    Added KeyGenGui.py
    KeyGen.py with a GUI.

---
+++ keygengui.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg      
+import random
+import hashlib
+adj = [
+    'needless',
+    'overwrought',
+    'changeable',
+    'abnormal',
+    'living',
+    'thoughtful',
+    'scattered',
+    'devilish',
+    'yellow',
+    'loud',
+    'foregoing',
+    'gruesome',
+    'weary',
+    'tiny',
+    'willing',
+    'thoughtful',
+    'educated',
+    'glorious',
+    'majestic',
+    'godly'
+]
+noun = [
+    'kitten',
+    'father',
+    'chicken',
+    'machine',
+    'dad',
+    'bat',
+    'sock',
+    'tiger',
+    'actor',
+    'cat',
+    'eggs',
+]
+
+x = random.choice(adj) + ', '  + random.choice(adj) + ', ' + random.choice(adj) + ' ' + random.choice(noun)
+y =  x.encode('utf-8')
+h = hashlib.md5(y).hexdigest()
+sg.popup(x + " " +  h, title="KeyGen", font=15)
