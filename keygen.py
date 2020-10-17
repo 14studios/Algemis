c3c0390e7ddf877de0e53989935c5b062fe06477
justinl681 <59315005+justinl681@users.noreply.github.com>
2020-10-17 19:05:08 -0400

    Added Keygen.py

---
+++ keygen.py
@@ -0,0 +1,42 @@
+import random
+import hashlib
+adj = [
+    'needless',
+    'overwrought',
+    'changeable',
+    'abnormal',
+    'living',
+    'thoughtful',
+    'scattered',
+    'devilish',
+    'yellow',
+    'loud',
+    'foregoing',
+    'gruesome',
+    'weary',
+    'tiny',
+    'willing',
+    'thoughtful',
+    'educated',
+    'glorious',
+    'majestic',
+    'godly'
+]
+noun = [
+    'kitten',
+    'father',
+    'chicken',
+    'machine',
+    'dad',
+    'bat',
+    'sock',
+    'tiger',
+    'actor',
+    'cat',
+    'eggs',
+]
+
+x = random.choice(adj) + ', '  + random.choice(adj) + ', ' + random.choice(adj) + ' ' + random.choice(noun)
+y =  x.encode('utf-8')
+h = hashlib.md5(y).hexdigest()
+print(h, x)
